d966d9bb1d565ab3775ffed2ccc3846b243de5e8
fascoche <joalland.fabien@gmail.com>
2019-01-27 14:12:53 +0100

    feat(population): generate a population

--- travelling_salesman_problem.py
+++ travelling_salesman_problem.py
@@ -5,7 +5,7 @@ import random
 Distances=[]
 
 #loading the CSV file containing the distances between capitals
-with open('base.csv', newline='') as csvfile:
+with open('base2.csv', newline='') as csvfile:
 	allDistances = csv.reader(csvfile, delimiter=';')
 	for index, row in enumerate(allDistances):
         
@@ -17,26 +17,36 @@ with open('base.csv', newline='') as csvfile:
 		else:
 		    del row[0]
 		    Distances.append(row)
-
-print(Capitals)
-print(Distances)
-print(len(Capitals))
-print(len(Distances))
 Data1 = pd.DataFrame(Distances, columns=Capitals, index=Capitals) # Dataframe for the capitals
 
-print(Data1.loc["Paris", "Bruxelles"])
-print(len(Data1.get("Paris")))
-
-print('-----')
+print('---Create Population--')
 #We start from Paris, so we already have this capital in our Path
-path = ['Paris']
-random.shuffle(Capitals)
-for index, capi in enumerate(Capitals):
-    for index, cap in enumerate(Capitals):
-        if(Data1.loc[path[len(path)-1],cap] != "-" and cap not in path):
-            print(Data1.loc[path[len(path)-1],cap])
-            path.append(cap)
-            break
-    
-print(path)
-print(len(path))
+
+for x in range(5000):
+    random.shuffle(Capitals)
+    path = ['Paris']
+    #We are looking for a path through all the cities (so the size of our table of capitals)
+    for x in range(len(Capitals)):
+        #Loop on the list of capitals randomly ranked
+        for index, cap in enumerate(Capitals):
+            if(len(path) == len(Capitals) - 1):
+                #print("Size of the current path {}, size of capitals {}".format(len(path), len(Capitals)))
+                if(Data1.loc[path[len(path)-1], cap]!= "" and Data1.loc[path[0], cap]!= "" and cap not in path):
+                    path.append(cap)
+                    path.append("Paris")
+            #If the proposed capital is a destination of the current capital, we add it to our solution
+            elif (Data1.loc[path[len(path)-1],cap] != "" and cap not in path):
+                path.append(cap)
+                
+    if(len(path)==18):
+        print("--------")
+        print(path)
+        
+        for index, geneCapital in enumerate(path):
+            if(index == len(Capitals)):  
+                print(geneCapital)
+                print('********')
+            else:
+                print(geneCapital + "-" + path[index+1])
+                print(Data1.loc[geneCapital,path[index+1]])
+           
