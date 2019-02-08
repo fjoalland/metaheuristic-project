de92d5c2918bb4754c97ab1c1374b8f3d4a75591
fascoche <joalland.fabien@gmail.com>
2019-02-08 20:53:51 +0100

    feat(data): full csv file)

--- travelling_salesman_problem.py
+++ travelling_salesman_problem.py
@@ -31,7 +31,7 @@ incrementPath=0
 #******GENERATION OF THE POPULATION******
 #****************************************
 print('---Create Population--')
-for x in range(20000):
+for x in range(1000):
     #In order to have genes of random individuals, capitals are randomly arranged.
     random.shuffle(Capitals)
     #We start from Paris, so we already have this capital in our Path
