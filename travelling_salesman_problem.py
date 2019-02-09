d1150e608b4630c8f4d8b456c073d7344dbb2eec
fascoche <joalland.fabien@gmail.com>
2019-02-09 12:31:22 +0100

    feat(crossover): dna crossover

--- travelling_salesman_problem.py
+++ travelling_salesman_problem.py
@@ -1,9 +1,26 @@
 import csv
 import pandas as pd
 import random
+
 #Contains distances between capitals
 Distances=[]
+
+#The beginning city and the point of arrival
 firstCity="Paris"
+
+#Number of cities
+cityCount=18
+
+#This corresponds to the percentage of mortality of individuals. 
+#The stronger an individual, the more likely he is to stay alive and reproduce
+individualsTournamentCount=5
+
+listIdentifierUser = []
+
+#The chance percentage that allows an elite to survive the next generation
+survivalElite=0.10
+
+
 #**********************************
 #******LOADING THE CSV FILE********
 #**********************************
@@ -25,13 +42,14 @@ with open('base2.csv', newline='') as csvfile:
 # Dataframe for the capitals
 intercapitalDistanceData = pd.DataFrame(Distances, columns=Capitals, index=Capitals) 
 listScore=[]
+
 population=[]
 incrementPath=0
 #****************************************
 #******GENERATION OF THE POPULATION******
 #****************************************
 print('---Create Population--')
-for x in range(1000):
+for x in range(20000):
     #In order to have genes of random individuals, capitals are randomly arranged.
     random.shuffle(Capitals)
     #We start from Paris, so we already have this capital in our Path
@@ -50,9 +68,9 @@ for x in range(1000):
             elif (intercapitalDistanceData.loc[path[len(path)-1],cap] != "" and cap not in path):
                 path.append(cap)
                 
-    if(len(path)==18):
+    if(len(path)==cityCount):
         #print("--------")
-        print(path)
+        #print(path)
         score=0
         for index, geneCapital in enumerate(path):
             if(index != len(Capitals)):  
@@ -73,14 +91,110 @@ for x in range(1000):
 
 listScore.sort()
 print(len(listScore))
-#print(listScore)
+print(listScore)
 
+scoreCount = len(listScore)
 
+def calculateScore(candidate, id):
+    score=0
+    for index, geneCapital in enumerate(candidate):
+        if(index != len(Capitals)):  
+            #print(geneCapital + "-" + candidate[index+1])
+            listOfDistance = intercapitalDistanceData.loc[geneCapital,candidate[index+1]].split("/")
+            if("-" in listOfDistance):
+                listOfDistance.remove("-")
+                if("-" in listOfDistance):
+                    listOfDistance.remove("-")
+            listOfDistance.sort()
+            #print(listOfDistance[0])
+            if(listOfDistance[0] == ''):
+                score = 0
+                break
+            else:
+                score=score+int(listOfDistance[0])
+    
+    if(score != 0):
+        listScore.append([score, id])
+        
+    return score
+    
+def crossover(parent1Id, parent2Id):
+    
+    winner1 = tournament(populationScore[parent1Id])
+    winner2 = tournament(populationScore[parent2Id])
+    
+    parent1 = population[winner1[1]].copy()
+    parent2 = population[winner2[1]].copy()
+    
+    if(winner1[0]>winner2[1]):
+        crossOverPoint=random.randint(1,cityCount-1)
+    else:
+        crossOverPoint=random.randint(1,cityCount-6)
+    
+    crossOverPartOne = parent1[1:crossOverPoint-1]
+
+    for index, geneCapital in enumerate(crossOverPartOne):
+        parent2.remove(geneCapital)
+    
+    enfant = parent2[0:len(parent2)-1] + crossOverPartOne + [firstCity] 
+    return enfant
+
+#This function takes into account a candidate and will make him face several fighters. 
+#The outgoing winner is the one with the highest score
+def tournament(candidate):
+    parent = candidate
+    for x in range(individualsTournamentCount):
+        fighter = random.choice(populationScore)
+        if(parent[0] > fighter[0]):
+            parent = fighter
+    return parent
+        
 
 
 #****************************************
 #**********MATINF OF INDIVIDUALS*********
 #****************************************
-for index, individu in enumerate(listScore):
-    charismaticRay=len(population)//random.randint(7,12)
+
+
+
+initial = listScore.copy()
+
+
+
+for x in range(50):
+    listScore.sort()
+    populationScore = listScore.copy()
+    populationScore.sort()
+
+    listScore = []
+    incrementPop = 0
+    scoreCount = len(populationScore)
+
+    for index, individu in enumerate(populationScore):
+        pathParent1= []
+        pathParent2= []
+    
+        if(index != scoreCount-1):
+    
+            enfant = crossover(index, index + 1)
+            calculateScore(enfant, index)
+    
+
+print(initial)
+print('******************************')
+print('******************************')
+print('******************************')
+
+listScore.sort()
+
+print(listScore)
+
+
+
+
+        
     #print(min(((k, v[0]) for k, v in listScore), key=lambda key:min(individu[0])))
+    
+
+    
+    
